058d9acb4446b9866b13f22fa10d2669b95e8a0f
Newston <Newston45@gmail.com>
2024-04-03 19:27:53 +0300

    add check Roman val func

--- main.go
+++ main.go
@@ -8,55 +8,103 @@ import (
 	"strings"
 )
 
-func checkNumVal(val int) bool {
-	if val < 1 || val > 10 {
-		return false
+var arabicToRoman = map[int]string{
+	1:  "I",
+	2:  "II",
+	3:  "III",
+	4:  "IV",
+	5:  "V",
+	6:  "VI",
+	7:  "VII",
+	8:  "VIII",
+	9:  "IX",
+	10: "X"}
+
+var romanToArabic = map[string]int{
+	"I":    1,
+	"II":   2,
+	"III":  3,
+	"IV":   4,
+	"V":    5,
+	"VI":   6,
+	"VII":  7,
+	"VIII": 8,
+	"IX":   9,
+	"X":    10}
+
+func checkAndConvertToArabic(num string) (int, bool) {
+	isRoman := false
+
+	resNum, _ := strconv.Atoi(num) // получаем число int
+	if resNum == 0 {
+		resNum = romanToArabic[num]
+		isRoman = true
+		return resNum, isRoman
 	}
-	return true
+	return resNum, isRoman
+}
+
+func findTwoNumsAndOperator(Text string) (int, int, string) { // находим оператор и два числа
+	operators := []string{"+", "-", "/", "*"} // возможные арифметические операции
+
+	Text = strings.Trim(Text, "\n")          // удаляем перенос строки из записанной строки
+	Text = strings.ReplaceAll(Text, " ", "") // удаляем все пробелы
+
+	var operator string
+	for _, op := range operators { // проходимся циклом по возможным операциям
+		if strings.Contains(Text, op) { // и проверяем есть ли в введной строке символ операции
+			operator = op // если нашли записываем оператор
+			break
+		}
+	}
+
+	nums := strings.Split(Text, operator) // убираем оператор, получаем два числа из строки
+
+	fmt.Println(nums)
+
+	var num1isRoman, num2isRoman bool
+	num1, num1isRoman := checkAndConvertToArabic(nums[0])
+	num2, num2isRoman := checkAndConvertToArabic(nums[1])
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num1isRoman)
+	fmt.Println(num2isRoman)
+
+	if num1isRoman == true && num2isRoman == true {
+
+	}
+
+	return num1, num2, operator
+
+}
+
+func calculate(num1, num2 int, operator string) int { // выполнение математичских операций
+	switch operator {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		if num2 == 0 {
+			panic("На ноль делить нельзя!")
+		}
+		return num1 / num2
+	default:
+		panic("Неверная операция!")
+	}
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	operators := []string{"+", "-", "/", "*"} // возможные арифметические операции
-	var result int
-
 	fmt.Println("Это калькулятор!")
 
 	for {
-		fmt.Println("Введите что вы хотите посчитать.")
+		fmt.Println("Введите математическую операцию c двумя целыми числами:")
+		text, _ := reader.ReadString('\n') // записываем введенную пользователем строку
 
-		text, _ := reader.ReadString('\n')       // записываем введенную пользователем строку
-		text = strings.Trim(text, "\n")          // удаляем перенос строки из записанной строки
-		text = strings.ReplaceAll(text, " ", "") // удаляем все пробелы
-
-		var operator string
-		for _, op := range operators { // проходимся циклом по возможным операциям
-			if strings.Contains(text, op) { // и проверяем есть ли в введной строке символ операции
-				operator = op // если нашли записываем оператор
-				break
-			}
-		}
-
-		nums := strings.Split(text, operator) // убираем оператор, находим два числа из строки
-		num1, _ := strconv.Atoi(nums[0])      // получаем первое число int
-		num2, _ := strconv.Atoi(nums[1])      // получаем второе число int
-
-		switch operator {
-		case "+":
-			result = num1 + num2
-		case "-":
-			result = num1 - num2
-		case "*":
-			result = num1 * num2
-		case "/":
-			if num2 != 0 {
-				result = num1 / num2
-			}
-			panic("На ноль делить нельзя!")
-		}
-
-		fmt.Println("Результат вычисления: ", result)
-
+		fmt.Println("Результат вычисления: ", calculate(findTwoNumsAndOperator(text)))
 	}
 
 }
